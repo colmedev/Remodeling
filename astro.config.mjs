3d563e998b021703f8a8b4e7f909b3d7502682c7
colmedev <m.colmenarez.dev@gmail.com>
2024-10-18 18:32:09 -0400

    chore: :package: add packages to the project

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
 import { defineConfig } from 'astro/config';
+import liciousI18n from '@astrolicious/i18n';
+
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [liciousI18n({
+    defaultLocale: 'en',
+    locales: ['en', 'es'],
+    client: {
+      data: true,
+      paths: true,
+      translations: true
+    }
+  })]
+});

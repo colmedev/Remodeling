8825d72ce0b31fdb95e9d488b6140f8c90b4d389
colmedev <m.colmenarez.dev@gmail.com>
2024-10-18 18:36:20 -0400

    feat: :sparkles: add types to the project

---
+++ src/components/Navbar/data.ts
@@ -0,0 +1,26 @@
+import type { LinkItem } from "@contracts/link.types";
+/**
+ * Elementos de la barra de navegación
+ */
+export const NavItems: LinkItem[] = [
+  {
+    text: "Home",
+    url: "/",
+    label: "home",
+  },
+  {
+    text: "Services",
+    url: "/services",
+    label: "services",
+  },
+  {
+    text: "About",
+    url: "/about",
+    label: "about",
+  },
+  {
+    text: "Contact",
+    url: "/contact",
+    label: "contact",
+  },
+];
---
+++ src/contracts/link.types.ts
@@ -0,0 +1,22 @@
+/*
+ * Definición de elemento enlace
+ */
+
+export type LinkItem = {
+  /*
+   * URL del elemento
+   */
+  url?: string;
+
+  /*
+   * Etiqueta del elemento
+   */
+  label: string;
+};
+
+export type IconItem = {
+  /*
+   * URL del elemento
+   */
+  icon: string;
+};
